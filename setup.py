f7dc6a0eee17a99f93668bb2090fa8ed437b3f3d
austin-mroz <austin.mroz@gmail.com>
2024-07-04 19:29:41 +0100

    Update dependencies

--- setup.py
+++ setup.py
@@ -6,22 +6,20 @@ setuptools.setup(
     author="Steven Bennett, Andrew Tarzia",
     author_email="s.bennett18@imperial.ac.uk",
     description=(
-        "Contains molecular optimizers and property calculators for "
-        "use with stk."
+        "Contains molecular optimizers and property calculators for " "use with stk."
     ),
     url="https://github.com/JelfsMaterialsGroup/stko",
     packages=setuptools.find_packages(),
     install_requires=(
-        'scipy',
-        'numpy',
-        'networkx',
-        'stk',
-        'rdkit-pypi',
+        "scipy",
+        "numpy",
+        "networkx",
+        "stk",
     ),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.9',
+    python_requires=">=3.9",
 )
